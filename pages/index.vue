fff2a6b6c35470c4e1749d90505296224fd27219
dongsun1 <le12352@naver.com>
2022-10-18 20:56:37 +0900

    tailwind 설치

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,28 @@
 <template>
-  <Tutorial/>
+  <section>
+    <h1>Travel Manager</h1>
+    <b-form-input v-model="login.id" placeholder="아이디"></b-form-input>
+    <b-form-input
+      v-model="login.password"
+      placeholder="비밀번호"
+    ></b-form-input>
+    <b-button variant="info">로그인</b-button>
+    <hr />
+    <b-button variant="info">회원가입</b-button>
+  </section>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import Vue from "vue";
 
 export default Vue.extend({
-  name: 'IndexPage'
-})
+  data() {
+    return {
+      login: {
+        id: "",
+        password: "",
+      },
+    };
+  },
+});
 </script>
