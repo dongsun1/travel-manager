0d30d561932d69a44f82e7ba74c8dcc5fa3079bb
dongsun1 <le12352@naver.com>
2022-10-19 21:21:10 +0900

    alert component

---
+++ components/alert.vue
@@ -0,0 +1,33 @@
+<template>
+  <b-alert
+    class="w-75 position-absolute text-center"
+    style="bottom: 20%"
+    :show="dismissCountDown"
+    fade
+    variant="warning"
+    @dismissed="dismissCountDown = 0"
+    @dismiss-count-down="countDownChanged"
+  >
+    {{ text }}
+  </b-alert>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  props: {
+    text: { type: String, default: "", required: true },
+  },
+  data() {
+    return {
+      dismissSecs: 2,
+      dismissCountDown: 0,
+    };
+  },
+  methods: {
+    countDownChanged(dismissCountDown: number) {
+      this.dismissCountDown = dismissCountDown;
+    },
+  },
+});
+</script>
